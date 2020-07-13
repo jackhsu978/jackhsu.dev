8fdd37fe15c2a46b993a02fafe53e37c0c1468ae
Jack Hsu <jackhsu@pinterest.com>
2020-07-13 16:49:43 -0700

    image should be transparent background

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,7 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
+              backgroundColor: "transparent",
               maxWidth: 650, // default
             },
           },
